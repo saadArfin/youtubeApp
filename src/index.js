5bd4286423e1a93f2d7d4ad7787f5cada364d85e
saadArfin <saad.arfin.ug21@nsut.ac.in>
2024-09-26 16:30:59 +0530

    utils configured

--- src/index.js
+++ src/index.js
@@ -3,13 +3,26 @@
 // import express from "express"
 import dotenv from "dotenv";
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 // const app = express()
 
 dotenv.config({
     path: "./env",
 });
 
+//Approach 2
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed!!!", err)
+})
+
+
+
 //Approach 1
 // ( async () => {
 //     try{
